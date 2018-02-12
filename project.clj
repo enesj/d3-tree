74636bed8ee2eccfdd97f415519c4bb77f7fa273
enesj <enes.jakic@gmail.com>
2018-02-12 11:39:25 +0100

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject d3-tree "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :url "http://www.bas.gov.ba/jus"
   :license {:name "Eclipse Public License"
             :url  "http://www.eclipse.org/legal/epl-v10.html"}
 
